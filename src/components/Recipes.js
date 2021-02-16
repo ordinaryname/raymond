1b35568601d66f581d3168398a1d8ba07c82ff3d
ordinaryname <ordinaryapple@gmail.com>
2021-02-15 23:52:59 -0700

    Refactor recipes hompage

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import './Recipes.css';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -11,12 +12,12 @@ class Recipes extends React.Component {
         <div className="recipes-homepage">
           <h1 className="recipes-header">Simple & Easy Recipes</h1>
           <h2 className="recipes-header2">Breakfast, Lunch, Snacks, Dinner, Drinks, and Late Night Munchies. Find it all here.</h2>
-          <div className="category recipes-breakfast">Breakfast</div>
-          <div className="category recipes-lunch">Lunch</div>
-          <div className="category recipes-snacks">Snacks</div>
-          <div className="category recipes-dinner">Dinner</div>
-          <div className="category recipes-latenight">Late Night Snacks</div>
-          <div className="category recipes-drinks">Drinks</div>
+          <div className="category recipes-breakfast"><span className="recipes-category-label">Breakfast</span></div>
+          <div className="category recipes-lunch"><span className="recipes-category-label">Lunch</span></div>
+          <div className="category recipes-snacks"><span className="recipes-category-label">Snacks</span></div>
+          <div className="category recipes-dinner"><span className="recipes-category-label">Dinner</span></div>
+          <div className="category recipes-latenight"><span className="recipes-category-label">Late Night Snacks</span></div>
+          <div className="category recipes-drinks"><span className="recipes-category-label">Drinks</span></div>
         </div>
         <Footer />
       </div>
